c5a3cb1b45b1eee6b4120a11a7855af63cf226b7
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-03-16 12:38:07 +0530

    feat: enhance footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,16 @@
+import { HeartIcon } from "lucide-react";
+
 export const Footer = () => {
   return (
-    <div className="py-5 flex flex-col items-center">
-      <p className=" text-gray-500">
+    <div className="flex flex-col items-center w-full py-8 text-white bg-red-500">
+      <p className="">
         &copy; {new Date().getFullYear()}. All rights reserved.
       </p>
 
-      <div className="text-lg">
-        <span> made with ❤️ by </span>
-        <a href="https://gh.ahmadullah.in" className="text-blue-500">
+      <div className="flex items-center gap-1 text-lg font-semibold">
+        <span>Made with</span>
+        <HeartIcon /> by
+        <a href="https://gh.ahmadullah.in" className="text-white underline">
           @aumirza
         </a>
       </div>
